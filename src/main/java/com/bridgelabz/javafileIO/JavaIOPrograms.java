965fed106360ac0e872f082750130f06a93c5346
AshokKoda <a.koda59@gmail.com>
2022-04-23 09:12:58 +0530

    ADded:JAVAIO Problems

---
+++ src/main/java/com/bridgelabz/javafileIO/JavaIOPrograms.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.javafileIO;
+
+public class JavaIOPrograms {
+
+	public static void main(String[] args) {
+		System.out.println("***************** JAVA IO *****************");
+
+	}
+
+}
